ce8915b158a37cb21f94d8a3c75c1ecacd84d45a
ShuchikaNath <nath.shuchika@gmail.com>
2023-09-26 15:36:35 -0400

    Committing api gateway and discovery server and service communication code

--- microservices-parent/microservices-new/inventory-service/src/main/java/com/microservices/inventoryservice/model/InventoryRepository.java
+++ microservices-parent/microservices-new/inventory-service/src/main/java/com/microservices/inventoryservice/model/InventoryRepository.java
@@ -3,6 +3,7 @@ package com.microservices.inventoryservice.model;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface InventoryRepository extends JpaRepository<Inventory,Long> {
@@ -10,4 +11,6 @@ public interface InventoryRepository extends JpaRepository<Inventory,Long> {
     @Query(value = "select * from t_inventory i where i.sku_code=?1",
             nativeQuery = true )
     Optional<Inventory> findBySkuCode(String skuCode);
+
+    List<Inventory> findBySkuCodeIn(List<String> skuCode);
 }
--- microservices-parent/microservices-new/order-service/src/main/java/com/microservices/orderservice/service/OrderService.java
+++ microservices-parent/microservices-new/order-service/src/main/java/com/microservices/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.microservices.orderservice.service;
 
+import com.microservices.orderservice.dto.InventoryResponse;
 import com.microservices.orderservice.dto.OrderLineItemsDto;
 import com.microservices.orderservice.dto.OrderRequest;
 import com.microservices.orderservice.model.Order;
@@ -7,8 +8,10 @@ import com.microservices.orderservice.model.OrderLineItems;
 import com.microservices.orderservice.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import javax.transaction.Transactional;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -19,6 +22,9 @@ public class OrderService {
 
     @Autowired
     private OrderRepository orderRepository;
+
+    @Autowired
+    private WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest){
 
@@ -31,7 +37,26 @@ public class OrderService {
 
         order.setOrderLineItemsList(orderLineItemsList);
 
-        orderRepository.save(order);
+      List<String> skuCodes = order.getOrderLineItemsList().stream().map(OrderLineItems::getSkuCode).collect(Collectors.toList());
+
+       //Call inventory service and place order if product is in stock
+        //to be able to read data from the response we use method : bodyToMono(<data type of response>)
+        //by default web client makes an asynchronous request ,
+        // to make synchronous request to inventory service  add method block()
+
+        InventoryResponse[] inventoryResponsesArray = webClientBuilder.build().get().uri("http://inventory-service/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode",skuCodes).build())
+                        .retrieve().bodyToMono(InventoryResponse[].class)
+                        .block();
+
+       boolean allProductsInStock =  Arrays.stream(inventoryResponsesArray).allMatch(InventoryResponse::isInStock);
+
+        if(allProductsInStock){
+            orderRepository.save(order);
+        }
+        else{
+            throw new IllegalArgumentException("Product is not in stock, please try again later");
+        }
 
     }
 

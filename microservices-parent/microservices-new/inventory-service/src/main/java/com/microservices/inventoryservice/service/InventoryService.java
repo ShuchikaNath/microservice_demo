d84a75ed35e29a50a01f647ccf99ea40a57a78d7
ShuchikaNath <nath.shuchika@gmail.com>
2023-09-28 10:29:40 -0400

    Committing spring security changes and circuit breakers

--- microservices-parent/microservices-new/inventory-service/src/main/java/com/microservices/inventoryservice/service/InventoryService.java
+++ microservices-parent/microservices-new/inventory-service/src/main/java/com/microservices/inventoryservice/service/InventoryService.java
@@ -2,6 +2,8 @@ package com.microservices.inventoryservice.service;
 
 import com.microservices.inventoryservice.dto.InventoryResponse;
 import com.microservices.inventoryservice.model.InventoryRepository;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,13 +13,22 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 public class InventoryService {
 
     @Autowired
     private InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
+    @SneakyThrows
     public List<InventoryResponse> isInStock(List<String> skuCode){
+
+        /* To Simulate slow behaviour of inventory service:
+        *
+        log.info("Wait started");
+        Thread.sleep(10000);
+        log.info("Wait ended");
+*/
         return inventoryRepository.findBySkuCodeIn(skuCode).stream()
                 .map(inventory ->
                     InventoryResponse.builder()
